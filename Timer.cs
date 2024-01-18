c10ee0066fe768cbecd726df63afa26a22ea6973
A1b2C3DAAA <150339510+A1b2C3DAAA@users.noreply.github.com>
2024-01-18 13:19:44 +0400

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public int minutes = 3;
+    public float seconds = 59.0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        if (seconds <= 0)
+        {
+            if (minutes > 0)
+            {
+                minutes -= 1;
+                seconds = 59.0f;
+            }
+            else
+            {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }   
+        
+
+
+    }
+}
